5c1be110a337d0f575049112e50257db11c6fc1f
Moritz Makowski <makowskimoritz@gmail.com>
2019-12-12 10:32:16 +0100

    Finished index page

--- react_frontend/src/App/IndexPage/IndexPage.jsx
+++ react_frontend/src/App/IndexPage/IndexPage.jsx
@@ -8,23 +8,36 @@ import {Link} from 'react-router-dom';
 
 /* Styling Imports --------------------------------------------------------------- */
 import './IndexPage.scss';
-
+import Breakpoint from 'react-socks';
 
 /* Material UI Imports ----------------------------------------------------------- */
 import {Button} from '@material-ui/core';
 
 
+/* Assets Imports ---------------------------------------------------------------- */
+import EVENT_LOGO from './images/EventLogo';
+
 /* Component --------------------------------------------------------------------- */
 
 
 export function IndexPage() {
-  return (
-    <div className="IndexPage">
-        <div>
-            <Button variant="contained" color="secondary">
-                <Link to={"/event"}>Learn More</Link>
-            </Button>
-        </div>
-    </div>
-  );
+	return (
+		<div className="IndexPage">
+			<Breakpoint small down>
+				<div className="Image"/>
+			</Breakpoint>
+			<Breakpoint medium up>
+				<video className="Video" autoPlay muted loop>
+					<source id="mp4" src="https://storage.googleapis.com/i14-worlds-2021-gallery/default-images/GCA_Teaser_Cut.mp4" type="video/mp4"/>
+				</video>
+			</Breakpoint>
+			<div className="CenterBox">
+                <div className="Logo Element">{EVENT_LOGO}</div>
+                <Button className="Element" variant="contained" color="secondary">
+					<Link to={"/event"}>Learn More</Link>
+				</Button>
+			</div>
+		</div>
+	);
+
 }
